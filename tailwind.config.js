d2750c8274e60f17e43b1dd2f90a0fdd739b6eaf
yang.qiqiang <yang.qiqiang@fujitsu.com>
2024-12-02 13:46:57 +0900

    06-004

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,6 @@ module.exports = {
     require("./plugins/openVariant"),
     require("./plugins/animationDelay"),
     require("./plugins/tableCaption"),
+    require("./plugins/buttonPlugin"),
   ],
 };
